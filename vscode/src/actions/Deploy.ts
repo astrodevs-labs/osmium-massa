42e8d306aabb5903194aec361ff55ba6bd80c0e2
ByFish <maringertheo@gmail.com>
2024-09-08 18:05:07 +0200

    feat: cleaning

--- vscode/src/actions/Deploy.ts
+++ vscode/src/actions/Deploy.ts
@@ -2,25 +2,8 @@ import { WalletRepository } from './WalletRepository.js';
 import { ContractParams } from './types.js';
 import { EnvironmentRepository } from './EnvironmentRepository.js';
 import { DeployContractRepository } from './DeployContractRepository.js';
-import {
-  Args,
-  ArgTypes,
-  CHAIN_ID,
-  Client,
-  ClientFactory,
-  fromMAS,
-  IContractData,
-  IProvider,
-  MAX_GAS_DEPLOYMENT,
-  ProviderType,
-  SmartContractsClient,
-  u64ToBytes,
-  u8toByte,
-  WalletClient,
-} from '@massalabs/massa-web3';
+import { CHAIN_ID, Client, ClientFactory, fromMAS, IProvider, ProviderType, WalletClient } from '@massalabs/massa-web3';
 import { readFileSync } from 'node:fs';
-import { deploySmartContracts } from '../utils';
-import path from 'node:path';
 
 export interface DeployContractOptions {
   environmentId: string;
@@ -68,44 +51,6 @@ export class Deploy {
       throw new Error(`wallet id ${walletId} not found`);
     }
 
-    //const datastore = new Map<Uint8Array, Uint8Array>();
-    //
-    //datastore.set(new Uint8Array([0x00]), u64ToBytes(BigInt(1)));
-    //datastore.set(u64ToBytes(BigInt(1)), readFileSync(contractInfos.path));
-    //
-    //if (params.length) {
-    //  const args = new Args();
-    //
-    //  for (const param of params) {
-    //    if (param.type === ArgTypes.STRING) args.addString(param.value);
-    //    if (param.type === ArgTypes.BOOL) args.addBool(param.value);
-    //    if (param.type === ArgTypes.U8) args.addU8(param.value);
-    //    if (param.type === ArgTypes.U32) args.addU32(param.value);
-    //    if (param.type === ArgTypes.U64) args.addU64(param.value);
-    //    if (param.type === ArgTypes.I128) args.addI128(param.value);
-    //    if (param.type === ArgTypes.U128) args.addU128(param.value);
-    //    if (param.type === ArgTypes.U256) args.addU256(param.value);
-    //    if (param.type === ArgTypes.I32) args.addI32(param.value);
-    //    if (param.type === ArgTypes.I64) args.addI64(param.value);
-    //    if (param.type === ArgTypes.F32) args.addF32(param.value);
-    //    if (param.type === ArgTypes.F64) args.addF64(param.value);
-    //    if (param.type === ArgTypes.UINT8ARRAY) args.addUint8Array(param.value);
-    //    if (param.type === ArgTypes.SERIALIZABLE) args.addSerializable(param.value);
-    //    if (param.type === ArgTypes.SERIALIZABLE_OBJECT_ARRAY) args.addSerializableObjectArray(param.value);
-    //  }
-    //
-    //  datastore.set(
-    //    new Uint8Array(new Args().addU64(BigInt(1)).addUint8Array(u8toByte(0)).serialize()),
-    //    new Uint8Array(args.serialize()),
-    //  );
-    //}
-    //if (value > 0) {
-    //  datastore.set(
-    //    new Uint8Array(new Args().addU64(BigInt(1)).addUint8Array(u8toByte(1)).serialize()),
-    //    u64ToBytes(fromMAS(value)), // scaled value to be provided here
-    //  );
-    //}
-    //
     const deployerAccount = await WalletClient.getAccountFromSecretKey(walletInfos.privateKey);
 
     const web3Client: Client = await ClientFactory.createCustomClient(
@@ -117,20 +62,6 @@ export class Deploy {
       true,
       deployerAccount,
     );
-    CHAIN_ID.BuildNet;
-    //
-    //console.log(fromMAS(fees))
-
-    // return await web3Client.smartContracts().deploySmartContract(
-    //   {
-    //     contractDataBinary: readFileSync(contractInfos.path),
-    //     fee: fromMAS(fees),
-    //     maxGas: MAX_GAS_DEPLOYMENT,
-    //     datastore,
-    //     maxCoins: fromMAS(value),
-    //   } as IContractData,
-    //   web3Client.wallet().getBaseAccount()!,
-    // );
 
     return await web3Client.smartContracts().deploySmartContract({
       fee: fromMAS(fees),
@@ -138,19 +69,5 @@ export class Deploy {
       maxGas: 0n,
       contractDataBinary: readFileSync(contractInfos.path),
     });
-
-    //return deploySmartContracts(
-    //  [
-    //    {
-    //      data: readFileSync(path.join(__dirname, '.', '/sc.wasm')),
-    //      coins: fromMAS(value),
-    //    },
-    //  ],
-    //  web3Client,
-    //  web3Client.wallet().getBaseAccount()!,
-    //  fromMAS(fees),
-    //  3000_000_000n,
-    //  fromMAS(value),
-    //);
   }
 }
